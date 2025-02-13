072e2520847ef6fed9460af01a5a42ee094f96fe
JowhaanS <johan.e.sepp@gmail.com>
2025-02-13 11:54:46 +0100

    removed excess

--- projekt/keygen.py
+++
@@ -1,9 +0,0 @@
-from cryptography.fernet import Fernet
-
-def generate_key():
-    key = Fernet.generate_key()
-    with open('secret.key', 'wb') as key_file:
-        key_file.write(key)
-    print('The key has been generated and saved as: "secret.key".')
-if __name__ == '__main__':
-    generate_key()
